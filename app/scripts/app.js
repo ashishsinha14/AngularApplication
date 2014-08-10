c377148187219b50f61c2b0d366b1a21dc8c8d13
HItesh Aneja <aneja_hitesh@yahoo.com>
2014-08-10 00:52:39 -0400

    Re-organized the project structure.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,30 @@
-'use strict';
+window.GPAC = {};
+GPAC.POC = {};
 
-/**
- * @ngdoc overview
- * @name angularAppApp
- * @description
- * # angularAppApp
- *
- * Main module of the application.
- */
-angular
-  .module('angularAppApp', []);
+GPAC.POC.Libs = angular.module('gpac.poc.libs', [
+    'ngRoute',
+    'ngGrid'
+]);
+GPAC.POC.Controllers = angular.module('gpac.poc.controllers', []);
+GPAC.POC.Directives = angular.module('gpac.poc.directives', []);
+GPAC.POC.Services = angular.module('gpac.poc.services', []);
+GPAC.POC.Filters = angular.module('gpac.poc.filters', []);
+GPAC.POC.Extensions = angular.module('gpac.poc.extensions', []);
+
+GPAC.POC.App = angular.module('gpacPOC', [
+        'gpac.poc.libs',
+        'gpac.poc.controllers',
+        'gpac.poc.directives',
+        'gpac.poc.services',
+        'gpac.poc.filters',
+        'gpac.poc.extensions'
+    ])
+
+.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'views/home.html'
+        });
+
+    $locationProvider.html5Mode(false);
+});
